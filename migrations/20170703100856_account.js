fbbb68ac4d5b6bd65be6df5d4b6a79a471fbc71c
JAKEFELDMAN <jacob.h.feldman@gmail.com>
2017-07-05 23:13:36 -0600

    Added CSV Data set

--- migrations/20170703100856_account.js
+++ migrations/20170703100856_account.js
@@ -2,6 +2,8 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('account', table => {
     table.increments('id').primary();
+    table.text('first_name').notNull();
+    table.text('last_name').notNull();
     table.text('email').notNull();
     table.text('password').notNull();
     table.text('image');
