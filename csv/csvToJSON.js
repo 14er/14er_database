d66ac0eefdeb0e96f07cdff37b2faf89cb5041e7
JAKEFELDMAN <jacob.h.feldman@gmail.com>
2017-07-05 23:48:15 -0600

    Updated Data

--- csv/csvToJSON.js
+++ csv/csvToJSON.js
@@ -24,3 +24,59 @@ fs.readFile('./account.csv', (error, data) => {
     });
   });
 });
+
+fs.readFile('./ranges.csv', (error, data) => {
+  csv.parse(data, (error, data) => {
+    let allRanges = [];
+    for (var i = 1; i < data.length; i++) {
+      const row = data[i];
+      const range = {
+        name: row[0]
+      }
+      allRanges.push(range)
+    }
+    fs.writeFile('../data-sets/ranges.json', JSON.stringify(allRanges), error => {
+      console.log("ranges.json created");
+    });
+  });
+});
+
+fs.readFile('./peaks.csv', (error, data) => {
+  csv.parse(data, (error, data) => {
+    let allPeaks = [];
+    for (var i = 1; i < data.length; i++) {
+      const row = data[i];
+      const peak = {
+        peak_name: row[1],
+        elevation: row[2],
+        image_url: row[4],
+        difficulty: row[3],
+        rank: row[0],
+        range_id: row[5]
+      }
+      allPeaks.push(peak)
+    }
+    fs.writeFile('../data-sets/peaks.json', JSON.stringify(allPeaks), error => {
+      console.log("peaks.json created");
+    });
+  });
+});
+
+fs.readFile('./account_peak.csv', (error, data) => {
+  csv.parse(data, (error, data) => {
+    let allAccountPeaks = [];
+    for (var i = 1; i < data.length; i++) {
+      const row = data[i];
+      const account_peak = {
+        account_rating: row[2],
+        account_image_url: row[3],
+        account_id: row[0],
+        peak_id: row[1]
+      }
+      allAccountPeaks.push(account_peak)
+    }
+    fs.writeFile('../data-sets/account_peak.json', JSON.stringify(allAccountPeaks), error => {
+      console.log("account_peak.json created");
+    });
+  });
+});
