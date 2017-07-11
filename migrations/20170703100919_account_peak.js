03f8e52a381e1082736f63a46dcd1ccfb23b37be
JAKEFELDMAN <jacob.h.feldman@gmail.com>
2017-07-10 21:03:20 -0600

    Updated Tables

--- migrations/20170703100919_account_peak.js
+++ migrations/20170703100919_account_peak.js
@@ -2,8 +2,11 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('account_peak', table => {
     table.increments('id').primary();
-    table.integer('account_rating')
+    table.integer('account_rating');
     table.text('account_image_url');
+    table.text('account_notes');
+    table.boolean('is_complete');
+    table.date('date_complete');
     table.integer('account_id').unsigned().references('id').inTable('account').onDelete('cascade');
     table.integer('peak_id').unsigned().references('id').inTable('peak').onDelete('cascade');
   });
